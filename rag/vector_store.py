33edb14e1d78f55700d48709f4506ddec3cd0450
Divi <divithasrianjani@gmail.com>
2026-05-13 19:38:22 +0530

    fixed requirements

--- rag/vector_store.py
+++ rag/vector_store.py
@@ -1,5 +1,5 @@
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
